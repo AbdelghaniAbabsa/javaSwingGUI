e398a758fc82869adbe5cb7149ca15510b5cf195
AbdelghaniAbabsa <abdelghaniababsa4@gmail.com>
2018-10-23 18:25:24 +0100

    Itiale Commit

---
+++ Project02.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project02;
+
+/**
+ *
+ * @author pc
+ */
+public class Project02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Home4 h = new Home4();
+        h.setVisible(true);
+    }
+    
+}
